150bbe270a2d834ab0bb97eda22b07348b76d32f
Dariusz Okonski <darek200180@gmail.com>
2021-08-21 00:45:40 +0100

    add delete funcionality

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -33,4 +33,15 @@ public class StudentService {
         studentRepository.save(student);
         System.out.println("student saved");
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+
+        if(!exists) {
+            throw new IllegalStateException(
+                    "student with id " + studentId + " does not exists");
+        }
+
+        studentRepository.deleteById(studentId);
+    }
 }
